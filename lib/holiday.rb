4d14046988f0ee2434325a6e33bdd0f8629f9576
Iskra B <ibatistapoblete@gmail.com>
2019-04-24 02:42:08 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,15 +68,12 @@ end
 
 
 def all_holidays_with_bbq(holiday_hash)
-  holiday_hash.each do |season, holiday|
+  holiday_hash.map do |season, holiday|
     holiday.each do |day, supply|
-      if supply == "BBQ"
-        return day
-      end
+      binding.pry
     end
   end
 end
 
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-end
