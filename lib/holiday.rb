b58d0524eb4a8abd9caec94afb11ab35dba3e3da
Iskra B <ibatistapoblete@gmail.com>
2019-04-24 02:33:10 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -53,7 +53,6 @@ end
 def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday| puts "#{season.capitalize}:"
     holiday.each do |day, supply| puts "#{day.to_s.split(" ").collect {|word| word.capitalize!}.join( " ")}: #{supply.join(" ")}"
-      binding.pry
     end
   end
 end
